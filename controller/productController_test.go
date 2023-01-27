41a9b2ea63b652ed1ff57777ddff45c0f1646ebc
mohdjishin <whoistcp@protonmail.com>
2023-01-27 20:03:45 +0530

    test product - jwt test

---
+++ controller/productController_test.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestSearchProduct(t *testing.T) {
+	type args struct {
+		c *fiber.Ctx
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SearchProduct(tt.args.c); (err != nil) != tt.wantErr {
+				t.Errorf("SearchProduct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
