cb488f7b893a0ba53f72db5db5f804ba1416616a
mohdjishin <whoistcp@protonmail.com>
2023-04-05 08:02:16 +0530

    interface updated

--- interfaces/IAdmin.go
+++ interfaces/IAdmin.go
@@ -3,14 +3,14 @@ package interfaces
 import "github.com/gofiber/fiber/v2"
 
 type IAdmin interface {
-	Signup(c *fiber.Ctx) error
-	Login(c *fiber.Ctx) error
-	Validate(c *fiber.Ctx) error
-	UserManagement(c *fiber.Ctx) error
-	ViewUsers(c *fiber.Ctx) error
-	Logout(c *fiber.Ctx) error
-	ViewOrders(c *fiber.Ctx) error
-	DeliveryStatusUpdate(c *fiber.Ctx) error
-	ManageUser(c *fiber.Ctx) error
-	AdminRefresh(c *fiber.Ctx) error
+	Signup(*fiber.Ctx) error
+	Login(*fiber.Ctx) error
+	Validate(*fiber.Ctx) error
+	UserManagement(*fiber.Ctx) error
+	ViewUsers(*fiber.Ctx) error
+	Logout(*fiber.Ctx) error
+	ViewOrders(*fiber.Ctx) error
+	DeliveryStatusUpdate(*fiber.Ctx) error
+	ManageUser(*fiber.Ctx) error
+	AdminRefresh(*fiber.Ctx) error
 }
--- interfaces/IUser.go
+++ interfaces/IUser.go
@@ -5,15 +5,15 @@ import "github.com/gofiber/fiber/v2"
 type IUser interface {
 	UserSignup(*fiber.Ctx) error
 	UserLogin(c *fiber.Ctx) error
-	Home(c *fiber.Ctx) error
-	Verification(c *fiber.Ctx) error
-	EditUserInfo(c *fiber.Ctx) error
-	AddToCart(c *fiber.Ctx) error
+	Home(*fiber.Ctx) error
+	Verification(*fiber.Ctx) error
+	EditUserInfo(*fiber.Ctx) error
+	AddToCart(*fiber.Ctx) error
 	OrderFromCart(c *fiber.Ctx) error
 	Checkout(c *fiber.Ctx) error
-	UserLogout(c *fiber.Ctx) error
-	Refresh(c *fiber.Ctx) error
-	GenerateInvoice(c *fiber.Ctx) error
-	RemoveFromCart(c *fiber.Ctx) error
-	First(c *fiber.Ctx) error
+	UserLogout(*fiber.Ctx) error
+	Refresh(*fiber.Ctx) error
+	GenerateInvoice(*fiber.Ctx) error
+	RemoveFromCart(*fiber.Ctx) error
+	First(*fiber.Ctx) error
 }
