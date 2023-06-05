4f92555ab9ea31c20f213a21199cfbd312255ed8
mohdjishin <whoistcp@pronmail.com>
2023-06-06 00:34:21 +0530

    interface updated

--- interfaces/IUser.go
+++ interfaces/IUser.go
@@ -4,13 +4,13 @@ import "github.com/gofiber/fiber/v2"
 
 type IUser interface {
 	UserSignup(*fiber.Ctx) error
-	UserLogin(c *fiber.Ctx) error
+	UserLogin(*fiber.Ctx) error
 	Home(*fiber.Ctx) error
 	Verification(*fiber.Ctx) error
 	EditUserInfo(*fiber.Ctx) error
 	AddToCart(*fiber.Ctx) error
 	OrderFromCart(c *fiber.Ctx) error
-	Checkout(c *fiber.Ctx) error
+	Checkout(*fiber.Ctx) error
 	UserLogout(*fiber.Ctx) error
 	Refresh(*fiber.Ctx) error
 	GenerateInvoice(*fiber.Ctx) error
